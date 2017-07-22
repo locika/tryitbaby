40005e709cbac437354a46897d107dce63e8bb8b
locika <szoyka.loci@gmail.com>
2017-07-22 14:09:28 +0200

    Test names added

--- cube.py
+++ cube.py
@@ -5,10 +5,13 @@ import unittest
 import cubeprogram
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_positive(self):
         self.assertEqual( "The V(olume) of the cube is: 8 The A(rea) of the cube is: 24", cubeprogram.cube(2))
+    def test_negative(self):
         self.assertEqual("The input parameter must be positive", cubeprogram.cube(-1))
+    def test_zero(self):
         self.assertEqual("The input parameter must be positive", cubeprogram.cube(0))
+    def test_string(self):
         self.assertEqual("The input cannot be converted to integer", cubeprogram.cube("asd"))
         print("Pass")
 if __name__ == '__main__':
