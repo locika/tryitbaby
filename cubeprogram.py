a23f400320d98ad9885981499154ac56184e432d
locika <szoyka.loci@gmail.com>
2017-07-22 14:16:46 +0200

    Cube ready

---
+++ cubeprogram.py
@@ -0,0 +1,12 @@
+def cube(x):
+    """
+    Here comes your part :D
+    The Unit test waits for theese strings as output:
+    "The V(olume) of the cube is: "x" The A(rea) of the cube is: "y" "
+    "The input parameter must be positive"
+    "The input cannot be converted to integer"
+    """
+
+if __name__ == '__main__':
+    x=1#Change 1 to number what you want to test
+    cube(x)
