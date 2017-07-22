ee7dcc52ecd513e96327e25f1f800dd9ae03d8df
locika <szoyka.loci@gmail.com>
2017-07-22 13:40:08 +0200

    added new test

--- cube.py
+++ cube.py
@@ -8,6 +8,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual( "The V(olume) of the cube is: 8 The A(rea) of the cube is: 24", cubeprogram.cube(2))
         self.assertEqual("The input parameter must be positive", cubeprogram.cube(-1))
+        self.assertEqual("The input parameter must be positive", cubeprogram.cube(0))
         self.assertEqual("The input cannot be converted to integer", cubeprogram.cube("asd"))
         print("Pass")
 if __name__ == '__main__':
