dc3790d42d98e67c29315013362832deaee2a9f4
locika <30343936+locika@users.noreply.github.com>
2017-07-24 13:34:49 +0200

    Update triangleprogram.py

--- triangleprogram.py
+++ triangleprogram.py
@@ -6,7 +6,7 @@ def triangle(a,b,c):
     Here comes your part :D
     The Unit test waits for theese strings as output:
     "The T(erritory) of the triangle is: "x" 
-    "This is not a triangle"
+    "The sum of two sides must be larger than the third."
     "The input parameter must be positive"
     "The input cannot be converted to integer"
     Note: you have to round the number till 2 decimals if the number is not whole
