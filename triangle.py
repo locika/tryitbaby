82be5f2fe390257e96fadefbe6e4481babd329b4
locika <30343936+locika@users.noreply.github.com>
2017-07-24 13:01:33 +0200

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,3 +1,16 @@
 """
 This program calculates the T(erritory) of a triangle
 """
+def sphere(x):
+    """
+    Here comes your part :D
+    The Unit test waits for theese strings as output:
+    "The T(erritory) of the triangle is: "x" 
+    "The input parameter must be positive"
+    "The input cannot be converted to integer"
+    Note: you have to round the number till 2 decimals if the number is not whole
+    """
+
+if __name__ == '__main__':
+    x=1#Change 1 to number what you want to test
+sphere(x)
