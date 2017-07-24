0b9d2d6ee9c0d963cb321cf0a8f6adb0ea2ff1f1
locika <30343936+locika@users.noreply.github.com>
2017-07-24 10:29:57 +0200

    Update sphereUT.py

--- sphereUT.py
+++ sphereUT.py
@@ -1,12 +1,13 @@
 """
-This program tests the A(rea) and V(olume) of a cube
+This program tests the A(rea) and V(olume) of a sphere
 """
 import unittest
 import sphereprogram
-
-class MyTestCase(unittest.TestCase):
+class Sphere(unittest.TestCase):
     def test_positive(self):
-        self.assertEqual( "The V(olume) of the sphere is: 33,49 The A(rea) of the sphere is: 50,24",sphereprogram.sphere(2))
+        self.assertEqual("The V(olume) of the sphere is: 33.49 The A(rea) of the sphere is: 50.24", sphereprogram.sphere(2))
+    def test_whole(self):
+        self.assertEqual("The V(olume) of the sphere is: 3349 The A(rea) of the sphere is: 33493333.33", sphereprogram.sphere(200))
     def test_negative(self):
         self.assertEqual("The input parameter must be positive", sphereprogram.sphere(-1))
     def test_zero(self):
@@ -15,4 +16,4 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual("The input cannot be converted to integer", sphereprogram.sphere("asd"))
 
 if __name__ == '__main__':
-unittest.main()
+    unittest.main()
